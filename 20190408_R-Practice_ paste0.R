d298704b0602f878111d3eac0e8b78f9ed627a76
Samuel Ahuno <ekwame001@gmail.com>
2019-04-08 22:23:30 +0000

    variants of paste/paste0 function

---
+++ 20190408_R-Practice_ paste0.R
@@ -0,0 +1,12 @@
+#I am  trying out paste/paste0 functions
+
+paste(1:23)
+paste0(5:19)
+
+
+paste(1:3, c("st", "nd", "rd")) #This gives you output with spaces
+paste0(1:3, c("st", "nd", "rd")) #This gives you output without spaces
+
+
+paste0("plantain chips", "code just messed up", c("2", 1))
+paste0(1:5, "I love Mondays")
