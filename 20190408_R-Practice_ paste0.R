ac72d65ae11428f0600879d8628c2d98a5e493cd
Samuel Ahuno <ekwame001@gmail.com>
2019-04-09 16:48:06 +0000

    paste and paste0 functions practice

--- 20190408_R-Practice_ paste0.R
+++ 20190408_R-Practice_ paste0.R
@@ -10,3 +10,7 @@ paste0(1:3, c("st", "nd", "rd")) #This gives you output without spaces
 
 paste0("plantain chips", "code just messed up", c("2", 1))
 paste0(1:5, "I love Mondays")
+
+nth <- paste0(1:12, c("st", "nd", "rd", rep("th", 9)))
+nth
+paste(month.abb, nth, sep = "-", collapse = ",")
