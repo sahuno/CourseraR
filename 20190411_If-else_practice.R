a4a65282c2c22f2ce47c131c6c71c39ee8893fd2
Samuel Ahuno <ekwame001@gmail.com>
2019-04-11 16:42:29 +0000

    New program/game for which team wins when user inputs a couples of number for both numbers

---
+++ 20190411_If-else_practice.R
@@ -0,0 +1,26 @@
+#control structures
+
+#using if and else to determine which teams win
+welcome <- readline(prompt = "Hey, would you want to play the which team team wins game? Enter Y/N to continue: ")
+
+if (welcome == "Y") {
+  scoreA <- readline(prompt = "Enter the recent score for team A: ")
+  scoreA <- as.integer(scoreA)
+  print(paste("Team A has", scoreA, "scores"))
+  
+  scoreB <- readline(prompt = "Enter the recent score for team B")
+  scoreB <- as.integer(scoreB)
+  
+  print(paste("Team B has", scoreB, "scores"))
+  
+  if(scoreA > scoreB){
+    print(paste("Hurray Team A wins"))
+  } else {
+    print(paste("Hurray Team B wins"))
+  }
+}
+  
+
+  
+
+
