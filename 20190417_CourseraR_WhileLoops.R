ead06ed1a7601910371ac862656ceb2fb11f1b9b
Samuel Ahuno <ekwame001@gmail.com>
2019-04-19 16:59:28 +0000

    while loops examples in R- Coursera

---
+++ 20190417_CourseraR_WhileLoops.R
@@ -0,0 +1,23 @@
+count <- 0
+
+while(count < 0){
+  print(count)
+  count <- count + 1
+}
+
+
+
+#z <- 4
+#while (z >= 3 && z <= 10){
+ # print (z)
+ # coin <- rbinom (1,1, 0.5)
+  
+  #if(coin == 1){
+   # z <- z+1
+ # } else {
+   # z < z -1  
+   # }
+#}
+
+
+##Break and next
